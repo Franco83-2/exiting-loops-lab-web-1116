8bcd0e0083f832d617602ca0c5cba710c7f8ba57
franco83-2 <franco.84@gmail.com>
2017-01-18 14:16:35 -0500

    Done.

--- index.js
+++ index.js
@@ -26,4 +26,3 @@ function findBy(array, findFn) {
   }
   return null
 }
-// findBy(array, findFn) which looks for a value in array based on the return value of findFn. Return null if the value isn't found.
